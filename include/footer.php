da5d9d904719f1e721a490c808bc3297e3eac496
Alex <anou_web@hotmail.com>
2022-10-03 03:18:51 +0200

    add search, account and admin page

---
+++ include/footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-opposition text-light p-4 text-center">
+    <div class="container">
+        Retro Game. Tous droits réservés © 2022
+    </div>
+</footer>
---
+++ include/header.php
@@ -0,0 +1,32 @@
+<header class="sticky-top">
+    <nav class="navbar navbar-dark bg-principal navbar-expand-md">
+        <div class="container">
+            <div class="navbar-brand">
+                <img src="/img/retro_game_trois_manettes" alt="logoRetroGame1" class="img_retro_game_1">
+                RETRO GAME
+            </div>
+
+            <div class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#menuDeroulant">
+                <span class="navbar-toggler-icon"></span>
+            </div>
+
+            <div class="collapse navbar-collapse" id="menuDeroulant">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="/home.html" class="nav-link active ">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/search.html" class="nav-link">Recherche</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/account.html" class="nav-link">Mon compte</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/admin.html" class="nav-link">Admin</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+</header>
